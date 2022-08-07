ff7a44fecda894ea942c79942eadd65e101069d1
Osireg17 <osireg@hotmail.com>
2022-08-07 20:49:48 +0100

    Finishing Touches

--- index.js
+++ index.js
@@ -72,10 +72,15 @@ app.get('/auth/logout', logout)
 
 
 
+let port = process.env.PORT;
+if (port == null || port == "") {
+port = 4000;
+}
 
 
 
 
-app.listen(4000, () => {
-    console.log('Server is running on port 4000');
-})
+app.listen(port, ()=>{
+    console.log('App listening...')
+    })
+    
